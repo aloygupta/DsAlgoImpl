560599fe0c16c5577a38ac775dd6288c9618d70c
Aloy Gupta <aloygupta1993@gmail.com>
2022-01-01 21:28:30 +0530

    [DSAL-16] Implementing Fibonacci

---
+++ src/onlinecoding/Fibonacci.java
@@ -0,0 +1,14 @@
+package onlinecoding;
+
+// https://leetcode.com/problems/fibonacci-number/
+public class Fibonacci {
+
+    public int fib(int n) {
+        if(n==0)
+            return 0;
+        else if(n ==1)
+            return 1;
+        else
+            return fib(n-1)+fib(n-2);
+    }
+}
